bed558b1862a414e63d6018faf86f9e2328b6691
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-09 06:51:46 +0530

    Header added

---
+++ src/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import "./Header.css";
+
+const defaultProps = {
+  bgcolor: "#e23636",
+  style: { width: "100%", height: "8rem" },
+  borderRadius: "0px 0px 30px 30px",
+};
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <Box {...defaultProps} borderColor="primary.main">
+        
+          <div className="head-text">Marvel World</div>
+        
+      </Box>
+    </div>
+  );
+};
+
+export default Header;
+
