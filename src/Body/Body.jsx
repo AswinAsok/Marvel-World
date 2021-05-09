b232573cc83da849fef922cb1a725d84f5c01371
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-09 09:25:43 +0530

    Data is getting loaded without break

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import "./Body.css";
 import SearchBox from "../SearchBox/SearchBox";
+import CryptoJS from "crypto-js";
+import axios from "axios";
 
 const defaultProps = {
   bgcolor: "#518cca",
@@ -14,6 +16,19 @@ const defaultProps = {
 
 const Body = () => {
   const [search, setSearch] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const PublicKey = "da8461dc7ffeb7b5eb1d538469d985c0";
+    const PrivateKey = "dc0194e8887ce630a6f41e28b75a5fd30cfd49cc";
+    var timeStamp = +new Date();
+    var md5Hash = CryptoJS.MD5(`${timeStamp}${PrivateKey}${PublicKey}`);
+    axios.get(
+      `https://gateway.marvel.com:443/v1/public/characters?apikey=${PublicKey}&ts=${timeStamp}&hash=${md5Hash}`
+    ).then(res => {
+      setData(res.data)
+    })
+  });
 
   return (
     <div className="body-container">
@@ -21,6 +36,7 @@ const Body = () => {
         <Box {...defaultProps} borderColor="primary.main">
           <Grid container direction="row" justify="center" alignItems="center">
             <SearchBox search={search} setSearch={setSearch} />
+            {console.log(data)}
           </Grid>
         </Box>
       </Grid>
